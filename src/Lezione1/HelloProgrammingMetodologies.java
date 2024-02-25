b778d2b82b9e1e06654040c5de8e3f21027f0f73
hide-ous <mattia.samory@gmail.com>
2024-02-25 16:57:35 +0100

    + initial commit

---
+++ src/Lezione1/HelloProgrammingMetodologies.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Lezione1;
+
+/**
+ * 
+ */
+public class HelloProgrammingMetodologies {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String name="hide";
+		System.out.println("Hello, "+name);
+	}
+
+}
