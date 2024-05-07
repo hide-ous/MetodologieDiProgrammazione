e10eca802e05a385bacd79f34ac5126a38c8bb00
hide-ous <mattia.samory@gmail.com>
2024-05-07 10:04:38 +0200

    * alternative calls with static method

--- src/lecture15/HomeAssistantTester.java
+++ src/lecture15/HomeAssistantTester.java
@@ -1,9 +1,13 @@
 package lecture15;
 
 public class HomeAssistantTester {
+	static void printInput(UserInput ui) {
+		System.out.println(ui.theInput);
+	}
 
 	public static void main(String[] args) {
-		ISkill<UserInput> greeter = t -> System.out.println(t.theInput);
+//		ISkill<UserInput> greeter = t -> System.out.println(t.theInput);
+		ISkill<UserInput> greeter = HomeAssistantTester::printInput;
 		HomeAssistant assistant = new HomeAssistant();
 		assistant.addSkill(greeter, "greet");
 		assistant.callSkills("greet", new UserInput());
